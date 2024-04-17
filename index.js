b05f6d8b2440aee08bcb0749e0e4b2d2124f50c9
rajitalluri17 <rajitalluri.4@gmail.com>
2024-04-17 19:55:08 +0530

    basic hello world with out importing react

---
+++ index.js
@@ -0,0 +1,8 @@
+// get the root
+const root = document.getElementById('root');
+// created element
+const p = document.createElement('p');
+p.innerText = "hello world from JS";
+
+//appended child in the root
+root.appendChild(p);// it doesn't have quotes for p
